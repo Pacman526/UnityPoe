d62982a25748625d41da4755fd89892786416ea8
Pacman526 <55444098+Pacman526@users.noreply.github.com>
2019-10-17 13:52:29 +0200

    Buildings Spawning
    Building spawning on the map

--- Poe/Assets/Scripts/GameEngine.cs
+++ Poe/Assets/Scripts/GameEngine.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class GameEngine : MonoBehaviour
 {
 
-    [SerializeField] GameObject[] options = new GameObject[2];
-    [SerializeField] static int MIN_X = -10, MAX_X = 10, MIN_Z = -10, MAX_Z = 10, UNITS = 10;
+    [SerializeField] GameObject[] options = new GameObject[4];
+    [SerializeField] static int MIN_X = -10, MAX_X = 10, MIN_Z = -10, MAX_Z = 10, UNITS = 10, BUILDINGS = 5;
     
     // Start is called before the first frame update
     void Start()
@@ -15,12 +15,23 @@ public class GameEngine : MonoBehaviour
         {
             CreateUnit();
         }
+
+        for (int k = 0; k < BUILDINGS; k++)
+        {
+            CreateBuilding();
+        }
     }
 
     void CreateUnit()
     {
         GameObject unit = Instantiate(options[Random.Range(0, 2)]);
         unit.transform.position = new Vector3(Random.Range(MIN_X, MAX_X), 0.5f, Random.Range(MIN_Z, MAX_Z));
+    }
+
+    void CreateBuilding()
+    {
+        GameObject building = Instantiate(options[Random.Range(2, 4)]);
+        building.transform.position = new Vector3(Random.Range(MIN_X, MAX_X), 1, Random.Range(MIN_Z, MAX_Z));
     }
 
     // Update is called once per frame
